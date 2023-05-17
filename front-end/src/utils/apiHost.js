2d84fe4a729395db7aa483baf58d4180b3ff12d3
Cozmu <www.jorgereis@gmail.com>
2023-05-17 18:37:08 -0300

    fix: ...

--- front-end/src/utils/apiHost.js
+++ front-end/src/utils/apiHost.js
@@ -1,3 +1,3 @@
-const PROTOCOL = process.env.API_PROTOCOL || 'http';
-const HOST = process.env.API_HOST || 'localhost:3001';
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
+const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001';
 export { PROTOCOL, HOST };
